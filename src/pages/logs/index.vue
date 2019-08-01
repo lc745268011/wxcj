2a930a18dff897e92b6f30914998d0786f96fad4
lc <chen_chen1995@qq.com>
2019-08-01 18:38:10 +0800

    发起抽奖页面

--- src/pages/logs/index.vue
+++ src/pages/logs/index.vue
@@ -1,61 +1,209 @@
 <template>
-  <div>
-      <swiper v-if="imgUrls.length > 0" indidator-dots="imgUrls.length > 1" >
-      <block v-for="(item, index) in imgUrls" :key="index" >
-        <swiper-item>
-          <image :src="item" mode="scaleToFill"></image>
-        </swiper-item>
-      </block>
-    </swiper>
-
-    <ul class="container log-list">
-      <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
-        <card :text="(index + 1) + ' . ' + log"></card>
-      </li>
-    </ul>
+  <div class="form">
+    <div v-for="(item,index) in formUpload" class="formlist">
+      <div class="upload">
+        <image class="uploadImg" mode="aspectFill center" :src="item.uploadImg"></image>
+        <button @click="upload" class="uploadBtn">更换图片</button>
+      </div>
+      <div class="vCenter inputPanel">
+        <icon type="cancel" v-show="index+1>1" :class="{'del':index+1>1}" @click="delUpload(index)"/>
+        <div>
+          <i-cell-group>
+            <i-cell :title="formUpload.length>1?index+1+'等奖名称':'奖品名称'">
+              <input slot="footer" i-class="uploadInput" :value="item.name" right placeholder="请输入奖品名称" />
+            </i-cell>
+            <i-cell title="请输入奖品名称">
+              <input slot="footer" i-class="uploadInput" :value="item.num" maxlength="10000" type="number" right placeholder="份数" />
+            </i-cell>
+          </i-cell-group>
+        </div>
+      </div>
+    </div>
+    <div class="center">
+      <button size="mini" class="newUpload" @click="newUpload()">+ 添加新奖项</button>
+    </div>
+    <i-panel title="开奖设置">
+      <i-cell-group>
+        <i-cell title="设置开奖条件" is-link>
+          <picker slot="footer" @change="bindPickerChange" :value="index" :range="array">
+            <view class="picker">
+              {{array[index]}}
+            </view>
+          </picker>
+        </i-cell>
+        <i-cell v-if="index==0" title="设置开奖时间" is-link>
+          <picker slot="footer" mode="multiSelector" @change="bindMultiPickerChange" :value="multiIndex" :range="newMultiArray">
+            <span>{{time}}</span>
+          </picker>
+        </i-cell>
+        <i-cell v-if="index==1" title="设置开奖人数">
+          <input slot="footer" type="number" :value="num" right placeholder="人数" maxlength="10000"/>
+        </i-cell>
+      </i-cell-group>
+    </i-panel>
   </div>
 </template>
 
 <script>
-import { formatTime } from '@/utils/index'
-import card from '@/components/card'
-
 export default {
-  components: {
-    card
-  },
-
   data () {
     return {
-      logs: [],
-      imgUrls: [
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/newsPicture/05558951-de60-49fb-b674-dd906c8897a6',
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/coursePicture/0fbcfdf7-0040-4692-8f84-78bb21f3395d',
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/management-school-picture/7683b32e-4e44-4b2f-9c03-c21f34320870'
-      ]
+      formUpload: [{
+        uploadImg: '/static/images/uploadBanner.jpg',
+        name: '',
+        num: ''
+      }],
+      array: ['按时间自动开奖', '按人数自动开奖', '手动开奖'],
+      index: 0,
+      time: '',
+      multiArray: [],
+      multiIndex: [0, 0, 0, 0, 0],
+      num: ''
     }
   },
-
-  created () {
-    let logs
-    if (mpvuePlatform === 'my') {
-      logs = mpvue.getStorageSync({key: 'logs'}).data || []
-    } else {
-      logs = mpvue.getStorageSync('logs') || []
+  computed: {
+    newMultiArray: () => {
+      let array = []
+      const date = new Date()
+      const years = []
+      const months = []
+      const days = []
+      const hours = []
+      const minutes = []
+      for (let i = 2018; i <= date.getFullYear() + 10; i++) {
+        years.push('' + i)
+      }
+      array.push(years)
+      for (let i = 1; i <= 12; i++) {
+        if (i < 10) {
+          i = '0' + i
+        }
+        months.push('' + i)
+      }
+      array.push(months)
+      for (let i = 1; i <= 31; i++) {
+        if (i < 10) {
+          i = '0' + i
+        }
+        days.push('' + i)
+      }
+      array.push(days)
+      for (let i = 0; i < 24; i++) {
+        if (i < 10) {
+          i = '0' + i
+        }
+        hours.push('' + i)
+      }
+      array.push(hours)
+      for (let i = 0; i < 60; i++) {
+        if (i < 10) {
+          i = '0' + i
+        }
+        minutes.push('' + i)
+      }
+      array.push(minutes)
+      return array
     }
-    this.logs = logs.map(log => formatTime(new Date(log)))
+  },
+  mounted () {
+    var date = new Date()
+    var Y = date.getFullYear()
+    var M = date.getMonth()
+    M = M + 1
+    if (M < 10) {
+      M = '0' + M
+    }
+    var d = date.getDate()
+    if (d < 10) {
+      d = '0' + d
+    }
+    var h = date.getHours()
+    if (h < 10) {
+      h = '0' + h
+    }
+    var m = date.getMinutes()
+    this.time = `${Y}-${M}-${d} ${h}:${m}`
+  },
+  methods: {
+    newUpload () {
+      this.formUpload.push({
+        uploadImg: '/static/images/uploadBanner.jpg',
+        name: '',
+        num: ''
+      })
+    },
+    delUpload (index) {
+      this.formUpload.splice(index, 1)
+    },
+    bindPickerChange (e) {
+      this.index = e.mp.detail.value
+    },
+    bindMultiPickerChange (e) {
+      this.multiIndex = e.target.value
+      const index = this.multiIndex
+      const year = this.newMultiArray[0][index[0]]
+      const month = this.newMultiArray[1][index[1]]
+      const day = this.newMultiArray[2][index[2]]
+      const hour = this.newMultiArray[3][index[3]]
+      const minute = this.newMultiArray[4][index[4]]
+      this.time = year + '-' + month + '-' + day + ' ' + hour + ':' + minute
+    }
   }
 }
 </script>
 
-<style>
-.log-list {
-  display: flex;
-  flex-direction: column;
-  padding: 40rpx;
+<style lang="scss" scoped>
+.center{
+  text-align: center;
+  margin: 15px;
 }
-
-.log-item {
-  margin: 10rpx;
+.formlist{
+  margin-bottom:15px; 
+}
+.upload{
+  width: 100%;
+  position: relative;
+  .uploadImg{
+    width: 375px;
+    height: 150px;
+  }
+  .uploadBtn{
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background: #c0b9b3;
+    color: #fff;
+    border: 0;
+    font-size: 12px;
+    border-radius: 10px;
+  }
+  .uploadBtn::after {
+    border: none
+  }
+}
+.newUpload{
+  border: 1px solid #999999;
+  color: #999;
+  width: auto;
+  border-radius: 0
+}
+.newUpload::after {
+  border: none;
+}
+.inputPanel{
+  background:#fff;
+  >div{
+    flex:1;
+  }
+  .del{
+    margin:0 0 0 15px;
+  }
 }
 </style>
+<style>
+.form .uploadInput{
+  border-bottom: 1px solid #e9e9e9;
+  position: static;
+}
+</style>
+
